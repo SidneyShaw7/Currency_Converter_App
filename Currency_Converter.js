c7a0f97c26e7d4358539bab36507d38321ac6055
Nikita Dmitriev <dmitriev.nikitos@gmail.com>
2023-05-29 11:02:11 -0400

    index.html/style.css - some changes were made, app.js - some code was added

---
+++ Currency_Converter.js
@@ -0,0 +1,17 @@
+// *****      SELECT ITEMS       *****
+const lsClearBtn = document.querySelector('.left-side-clear-btn');
+const rsClearBtn = document.querySelector('.right-side-clear-btn');
+const lsExtendBtn = document.querySelector('.left-side-extend-btn');
+const rsExtendBtn = document.querySelector('.right-side-extend-btn');
+const swipeBtn = document.querySelector('.center-swipe-btn');
+const leftUpInput = document.querySelector('.left-up-input');
+const rightUpInput = document.querySelector('.right-up-input');
+const leftDownInput = document.querySelector('.left-down-input');
+const rightDownInput = document.querySelector('.right-down-input');
+
+// *****       EVENT LISTENERS      *****
+
+
+
+
+// *****        FUNCTIONS       *****
